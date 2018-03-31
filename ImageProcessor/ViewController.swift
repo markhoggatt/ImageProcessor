3832343bd459bf7d76cdbd22d8024219f8b3b5c9
Mark Hoggatt <mark.hoggatt@paxton.co.uk>
2018-03-31 18:37:28 +0100

    Load image into Core Image structure.

--- ImageProcessor/ViewController.swift
+++ ImageProcessor/ViewController.swift
@@ -10,11 +10,49 @@ import Cocoa
 
 class ViewController: NSViewController
 {
+	@IBOutlet weak var targetImage: NSImageView!
+
 	override func viewDidLoad()
 	{
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
+		let openDialogue = NSOpenPanel()
+		openDialogue.prompt = "Load Image"
+		openDialogue.canChooseFiles = true
+		openDialogue.canChooseDirectories = false
+		openDialogue.allowedFileTypes = ["jpg"]
+		let fileResponse : NSApplication.ModalResponse = openDialogue.runModal()
+		let fileUrl : URL?
+		switch fileResponse
+		{
+		case .continue, .OK:
+			fileUrl = openDialogue.urls[0]
+		default:
+			fileUrl = nil
+		}
+
+		guard let imgUrl : URL = fileUrl
+			else
+		{
+			return
+		}
+
+		guard let uncorrectedImg = CIImage(contentsOf: imgUrl)
+		else
+		{
+			return
+		}
+
+		let currentOrientation = uncorrectedImg.properties["Orientation"] as! Int32
+		let img = uncorrectedImg.oriented(forExifOrientation: currentOrientation)
+		let treatedImg = NSImage(size: img.extent.size)
+		let treatedRect = NSRectFromCGRect(img.extent)
+		treatedImg.lockFocus()
+		img.draw(at: NSZeroPoint, from: treatedRect, operation: .copy, fraction: 1.0)
+		treatedImg.unlockFocus()
+
+		targetImage.image = treatedImg
 	}
 
 	override var representedObject: Any?
